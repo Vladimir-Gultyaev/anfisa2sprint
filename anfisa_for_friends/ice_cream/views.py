cb4d8134f76dc0a850d01e7a8470dac2880f53a2
Vladimir Gultyaev <tornadroid1@yandex.ru>
2025-12-06 15:36:17 +0300

    first

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
-    context = {}
+    ice_cream = IceCream.objects.get(pk=pk)
+    context = {
+        'ice_cream': ice_cream,
+    }
     return render(request, template, context)
 
 
