cf6821b1538db633990baea5f9bd4ba33f12c41e
Vladimir Gultyaev <tornadroid1@yandex.ru>
2025-12-06 12:24:43 +0300

    first

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,20 @@
+from django.db.models import Q
 from django.shortcuts import render
+
+from ice_cream.models import IceCream
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'title', 'description'
+    ).filter(
+        Q(is_on_main=True)
+        & Q(is_published=True)
+        | Q(title__contains='пломбир')
+        & Q(is_published=True)
+    )
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
